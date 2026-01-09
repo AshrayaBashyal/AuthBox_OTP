1b80077c488bff4d3ad656dc4ffdb84add98f991
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-09 19:55:47 +0545

    feat(auth): add RegisterSerializer with Django password validation and custom user creation

---
+++ apps/users/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth.password_validation import validate_password
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = ["email", "password", "name", "dob"]
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
