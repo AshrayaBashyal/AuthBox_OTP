00218288e0f518e696276a13cdfcbd9a7096326a
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-14 16:54:44 +0545

    feat(auth): add VerifyEmailView to validate OTP and activate user accounts

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,14 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import (
-    RegisterSerializer)
+    RegisterSerializer, LoginSerializer, OTPVerifySerializer,
+    ForgotPasswordSerializer, ResetPasswordSerializer
+)
+from apps.emails.services import send_verification_email_task, send_reset_password_email_task
+from .models import User
+from apps.otp.utils import verify_otp
+
+
 
 class RegisterView(APIView):
     def post(self, request):
@@ -9,3 +17,18 @@ class RegisterView(APIView):
         user = serializer.save()
         send_verification_email_task.delay(user.id)
         return Response({"msg": "User registered. Check your email for OTP verification."}, status=201)
+
+
+class VerifyEmailView(APIView):
+    def post(self, request):
+        serializer = OTPVerifySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        email = serializer.validated_data["email"]
+        otp = serializer.validated_data["otp"]
+        user = User.objects.get(email=email)
+        if verify_otp(user.otp_secret, otp):
+            user.is_verified = True
+            user.otp_secret = None
+            user.save()
+            return Response({"msg": "Email verified successfully."})
+        return Response({"error": "Invalid or expired OTP."}, status=400)
